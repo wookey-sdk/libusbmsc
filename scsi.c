b9fe21c42c8d849d3222799629402dcd54a69790
Philippe Thierry <phil@reseau-libre.net>
2020-02-12 10:17:58 +0100

    [update] modify the way the SCSI stack initialization is done at SetConfiguration time (missing first command read request)

--- scsi.c
+++ scsi.c
@@ -1583,6 +1583,12 @@ static void scsi_write_data10(scsi_state_t current_state, cdb_t * current_cdb)
     return;
 }
 
+mbed_error_t scsi_initialize_automaton(void)
+{
+    read_next_cmd();
+    return MBED_ERROR_NONE;
+}
+
 /*
  * SCSI Automaton execution
  */
